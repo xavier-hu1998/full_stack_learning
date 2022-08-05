c6e9b80b6bbd191525de210581fc98db34987795
xavier-hu1998 <xavierhu1998@gmail.com>
2022-08-05 23:00:17 +0800

    'guessNum'

--- complete-javascript-course/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,8 @@
 'use strict';
+console.log(document.querySelector(`.message`).textContent);
+
+//DOM Document Object Model: Structured representation of HTML documents.
+//Allows JS to access HTML elements & style
+//querySelector
+//.textContent
+//DOM is part of Web APIs, not part of JS
